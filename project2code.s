4f6c7a252d289374f8e06624ba90f551d10870f9
Tarracon <36968612+Tarracon@users.noreply.github.com>
2018-11-05 16:32:55 -0500

    Created text file for MIPS code
    Created main section and data section. Data contains variable max_input which contains the value 4. This value will be used to determine if the user_input is too large.

---
+++ project2code.s
@@ -0,0 +1,9 @@
+#Started CompOrg Project 2
+#project2code.s
+.data #Declare variables to be used in program
+     max_input: .space 4
+     error: .asciiz "Input too long"
+.text
+main:
+     
+          

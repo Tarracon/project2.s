d7e310b772bfb01fccccff4f08880d2e8a54be3d
Tarracon <36968612+Tarracon@users.noreply.github.com>
2018-11-16 20:34:01 -0500

    Update string_to_decimal.s
    Add label go_back_to_beginning, which'll return the program back to the start of the user string. This'll be used for my loops. Also start go_forward label.

--- string_to_decimal.s
+++ string_to_decimal.s
@@ -38,14 +38,19 @@ see_some_chars:
   lb $t1,0($t0)
   addi $t0, $t0, 1
   addi $t3, $t3, 1
-  beq $t1, 10, go_back_beginning
-  beq $t1, 0, go_back_beginning
+  beq $t1, 10, go_back_to_beginning
+  beq $t1, 0, go_back_to_beginning
   bne $t1, 32, see_some_chars
 see_some_more_chars_or_spaces:
-  lb $t1,0($t0)
-  addi $t0, $t0, 1
+  lb $t1,0($t0) #Load bit from string
+  addi $t0, $t0, 1 #Iterate through string
   addi $t3, $t3, 1
-  beq $t1, 10, go_back_beginning
-  beq $t1, 0, go_back_beginning
-  bne $t1, 32, Invalid_Base_Error
+  beq $t1, 10, go_back_to_beginning 
+  beq $t1, 0, go_back_to_beginning
+  bne $t1, 32, Invalid_Base_Error #if character isn't right base print error message
   j see_some_more_chars_or_spaces
+go_back__to_beginning:
+  sub $t0, $t0, $t3     #restarting the pointer in char_array back to first index
+  la $t3, 0             #reset the counter
+
+go_forward:

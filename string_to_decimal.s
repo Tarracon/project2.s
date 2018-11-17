e283b6f931d5edcba49bc01812b6896178018755
Tarracon <36968612+Tarracon@users.noreply.github.com>
2018-11-16 20:12:24 -0500

    Update and rename project2code.s to string_to_decimal.s
    Added main declaration for code. Added lines 7-12 that take in user's input

--- string_to_decimal.s
+++ string_to_decimal.s
@@ -4,3 +4,9 @@ invalid_base_number: .asciiz "Invalid base-35 number." #<----- replace with your
 input_empty: .asciiz "Input is empty."
 input_string: .space 40000
 .text
+main:
+
+  li $v0, 8  #get user_input
+  la $a0, input_string
+  li $a1, 40000
+  syscall
